bdf0bc9afd06a1ef21abe74e20193614654762bb
Kelsey M. Reese <kreese1@nd.edu>
2020-01-28 13:47:28 -0700

    Update REESE_ET_AL_2019.R

--- REESE_ET_AL_2019.R
+++ REESE_ET_AL_2019.R
@@ -2,8 +2,8 @@
 ## DYNAMIC COMMUNITIES ON THE MESA VERDE CUESTA
 ## KELSEY M. REESE, DONNA M. GLOWACKI, TIMOTHY A. KOHLER
 ## AMERICAN ANTIQUITY
-## Volume(Issue):PageStart-PageEnd
-## Date of Publication 2019
+## 84(4): 728--747
+## WINTER 2019, DOI:10.1017/aaq.2019.74
 ########################################################################################
 ## AUTHOR: KELSEY M. REESE
 ########################################################################################
